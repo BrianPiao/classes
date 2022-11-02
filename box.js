6c35a4e322b6c0a9c458988c0e34d7084bc0ab7b
BrianPiao <74799865+BrianPiao@users.noreply.github.com>
2022-11-02 12:50:24 -0800

    Add files via upload

---
+++ box.js
@@ -0,0 +1,15 @@
+AFRAME.registerComponent("box", {
+     schema: {
+          moveX: { type: "number", default: 1 }
+     },
+     tick: function () {
+          this.data.moveX = this.data.moveX + 0.01
+          //this.el.getAttribute(): get the current values of position attribute.
+          //this.el gives reference to the entity as an HTML element.
+          var p = this.el.getAttribute("position")
+          p.x = this.data.moveX
+
+          //this.el.setAttribute(): to set the updated value of the position attribute.
+        this.el.setAttribute("position" , {x:p.x,y:p.y,z:p.z})
+     }
+})
